260b6e9e70f8de5e42d574875b37050ec47432ee
Arpad G. Bondor <arpad.g.bondor@gmail.com>
2021-04-20 19:05:34 +0100

    airtable back

--- examples/3-airtable/app.js
+++ examples/3-airtable/app.js
@@ -1,1 +1,26 @@
-const result = document.querySelector('.result')
+const result = document.querySelector('.result');
+
+fetchData();
+
+async function fetchData() {
+  try {
+    const { data } = await axios('/api/3-airtable');
+    result.innerHTML = data
+      .map(
+        ({ name, url, price }) => `
+    <article class="product">
+      <img
+        src="${url}"
+        alt="${name}"
+      />
+      <div class="info">
+        <h5>${name}</h5>
+        <h5 class="price">£${price}</h5>
+      </div>
+    </article>`
+      )
+      .join('');
+  } catch (error) {
+    result.innerText = error.response.data;
+  }
+}
